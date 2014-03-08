da8a409a20c91dbf26ae0e0a75462b5ee6eeff9d
ronenbarak <ronen_barak@yahoo.com>
2014-03-09 00:56:19 +0200

    add missing files to the git repository( still not sure whay they have not checkin in the first place)

---
+++ src/Barak.VersionPatcher.TFS/RevisionVersionHelper.cs
@@ -0,0 +1,13 @@
+﻿using Barak.VersionPatcher.Engine;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace Barak.VersionPatcher.TFS
+{
+    public static class RevisionVersionHelper
+    {
+        public static VersionSpec GetSpec(this IRevisionVersion revisionVersion)
+        {
+            return (revisionVersion as TFSRevisionVersion).VersionSpec;
+        }
+    }
+}
---
+++ src/Barak.VersionPatcher.TFS/TFSRevisionVersion.cs
@@ -0,0 +1,15 @@
+﻿using Barak.VersionPatcher.Engine;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace Barak.VersionPatcher.TFS
+{
+    public class TFSRevisionVersion : IRevisionVersion
+    {
+        public VersionSpec VersionSpec { get; private set; }
+
+        public TFSRevisionVersion(VersionSpec versionSpec)
+        {
+            VersionSpec = versionSpec;
+        }
+    }
+}
